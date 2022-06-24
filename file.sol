1197781f106a9d3c3dedea6ca961f196734bc307
shubham vyas <81517539+hxyro@users.noreply.github.com>
2022-06-24 14:52:04 +0530

    Update file.sol

--- file.sol
+++ file.sol
@@ -1,55 +1,72 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+
 error NotOwner();
-contract place{
-    enum _Status {created,approved, rejected}
-    // _Status private _status;
+error PropertyDoesNotExist();
+
+contract PropertyContract{
+
+    enum propertyStatus {Created, Approved, Rejected}    
     uint256 private ID;
+    address immutable contractOwner;
     struct Property{
+        uint256 id;
+        address propertyOwner;
+        uint256 Price;
+        propertyStatus Status;
         string Details;
-        address Owner; //
-        uint256 Price; //
-        _Status Status;
-        uint CreatedAt;
-        uint UpdatedAt;
-        uint256 Id;
+        uint256 CreatedAt;
+        uint256 UpdatedAt;
     }
 
-    mapping(uint256 => Property[]) internal History;
-    mapping(uint256 => Property) internal currentProperty;
+    mapping(uint256 => Property[]) private propertyHistory;
+    mapping(uint256 => Property) public currentProperty;
+
     constructor(){
         ID = 0;
+        contractOwner = msg.sender;
     }
+
+    //auto increment Id by one
+    modifier autoIncrementId(){
+        ID += 1;
+        _;
+    }
+    // check if the caller is the property owner
     modifier onlyOwner(uint256 Id){
-        if(msg.sender != currentProperty[Id].Owner){
+        if(msg.sender != currentProperty[Id].propertyOwner){
             revert NotOwner();
         }
         _;
     }
+    // check if property exists
     modifier isPropertyExist(uint256 Id){
-
+        if(currentProperty[Id].id == 0){ // Each attribute in the default initialize property must be equal to zero
+            revert PropertyDoesNotExist();
+        }
+        _;
     }
-    function createProperty(string calldata details, uint256 price) public returns(uint256) {
-        _Status status = _Status.created;
-        ID += 1;
+
+    function createProperty(string calldata details, uint256 price) autoIncrementId external returns(uint256){
         Property memory newProperty = Property({
-                Details: details,
-                Owner: msg.sender,
-                Price: price,
-                Status: status,
-                CreatedAt: block.timestamp,
-                UpdatedAt: block.timestamp,
-                Id: ID
+            id: ID,
+            propertyOwner: contractOwner,
+            Price: price,
+            Status: propertyStatus.Created,
+            Details: details,
+            CreatedAt: block.timestamp,
+            UpdatedAt: block.timestamp
         });
         currentProperty[ID] = newProperty;
-        History[ID].push(newProperty);
+        propertyHistory[ID].push(newProperty);
         return ID;
     }
-    function updatePrice(uint256 Id, uint256 updatedprice) onlyOwner(Id) public {
+    
+    function updatePrice(uint256 Id, uint256 updatedprice) isPropertyExist(Id) onlyOwner(Id) public{
         currentProperty[Id].Price = updatedprice;
         currentProperty[Id].UpdatedAt = block.timestamp; 
-        History[ID].push(currentProperty[Id]);
+        propertyHistory[ID].push(currentProperty[Id]);
     }
 
 }
